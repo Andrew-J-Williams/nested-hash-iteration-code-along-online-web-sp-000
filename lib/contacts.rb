ec347f166349f0ffd15c68ed04ebb0c809e73365
Andrew Williams <1williamsaj@gmail.com>
2019-09-12 02:29:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -21,7 +21,7 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       contact_details.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice cream| ice cream == "strawberry"}
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
     end
